f2573564a2662dcc8b6d0bb963ce0c103933a2ae
Mueed Ur Rehman <mueedramay@users.noreply.github.com>
2019-09-05 21:59:22 -0400

    Update lab1code_servo.ino

--- Lab 1/lab1code_servo/lab1code_servo.ino
+++ Lab 1/lab1code_servo/lab1code_servo.ino
@@ -1,23 +1,27 @@
 int leftServo =6;
 int rightServo= 5;
 #include <Servo.h>
-Servo parallax;
+Servo parallax1;
+Servo parallax2;
 
 void setup() {
   pinMode(leftServo, OUTPUT);
   pinMode(rightServo, OUTPUT);
-  parallax.attach(leftServo);
-  parallax.attach(rightServo);
+  parallax1.attach(leftServo);
+  parallax2.attach(rightServo);
 }
 
 void loop() {
-    parallax.write(leftServo, 80);
-    paralax.write(rightServo, 40);
-    delay(1000);
-    parallax.write(leftServo, 90);
-    parallax.write(rightServo, 90);
+    parallax1.write(180); //wheel on the other side so it is flipped
+    parallax2.write(40);
+    delay(3000);
+    parallax1.write(90);
+    parallax2.write(90);
     delay(500);
-    parallax.write(leftServo, 80);
-    parallax.write(rightServo, 40);
-    delay(5000);
+    parallax1.write(180);
+    parallax2.write(90);
+    delay(1500);
+//    parallax1.write(80);
+//    parallax2.write(40);
+//    delay(5000);
 }
